b4ab7e4b6eef334c9f91c57641d0af78fff60401
Vennapureddy Mahathi <mahathireddy2006@gmail.com>
2026-02-10 20:33:21 +0530

    feat: fetc weather data with error handling

--- script.js
+++ script.js
@@ -3,3 +3,21 @@ const searchBtn = document.getElementById("searchBtn");
 const weatherCard = document.getElementById("weatherCard");
 
 const API_KEY = "YOUR_API_KEY";
+
+async function getWeather(city) {
+  weatherCard.style.display = "block";
+  weatherCard.innerHTML = "Loading...";
+
+  try {
+    const res = await fetch(
+      `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${API_KEY}`
+    );
+
+    if (!res.ok) throw new Error("City not found");
+
+    const data = await res.json();
+    displayWeather(data);
+  } catch (error) {
+    weatherCard.innerHTML = error.message;
+  }
+}
