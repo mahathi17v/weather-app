669b2867025cc65ad36f817e19736e7a4973eedf
Vennapureddy Mahathi <mahathireddy2006@gmail.com>
2026-02-10 22:54:36 +0530

    init: changes in async function

--- script.js
+++ script.js
@@ -2,11 +2,13 @@ console.log("JS file loaded");
 const cityInput = document.getElementById("cityInput");
 const searchBtn = document.getElementById("searchBtn");
 searchBtn.addEventListener("click", () => {
-  console.log("Search button clicked");
+  const city = cityInput.value.trim();
+  console.log("City being sent:", `"${city}"`);
+  getWeather(city);
 });
 const weatherCard = document.getElementById("weatherCard");
 
-const API_KEY = "YOUR_API_KEY";
+const API_KEY = "1268b67972b5fc6db9d443db899c87ae";
 
 async function getWeather(city) {
   weatherCard.style.display = "block";
@@ -14,13 +16,19 @@ async function getWeather(city) {
 
   try {
     const res = await fetch(
-      `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${API_KEY}`
+      `https://api.openweathermap.org/data/2.5/weather?q=${encodeURIComponent(
+        city
+      )}&units=metric&appid=${API_KEY}`
     );
 
-    if (!res.ok) throw new Error("City not found");
-
     const data = await res.json();
+
+    if (!res.ok) {
+      throw new Error(data.message); // <-- REAL API MESSAGE
+    }
+
     displayWeather(data);
+
   } catch (error) {
     weatherCard.innerHTML = error.message;
   }
