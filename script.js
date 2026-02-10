489a9497ceaf1aefd4216449755297e6bb776fa7
Vennapureddy Mahathi <mahathireddy2006@gmail.com>
2026-02-10 20:32:35 +0530

    init:setup DOM references and API key

---
+++ script.js
@@ -0,0 +1,5 @@
+const cityInput = document.getElementById("cityInput");
+const searchBtn = document.getElementById("searchBtn");
+const weatherCard = document.getElementById("weatherCard");
+
+const API_KEY = "YOUR_API_KEY";
